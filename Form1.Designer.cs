f784a029740eefbbba6b0df42387818d8fe88a5a
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-04-22 17:42:15 +0200

    v1.0.0.1

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,7 +30,9 @@ namespace CClick
         private void InitializeComponent()
         {
             this.clickableButton = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // clickableButton
@@ -50,35 +52,57 @@ namespace CClick
             this.clickableButton.UseVisualStyleBackColor = false;
             this.clickableButton.Click += new System.EventHandler(this.clickableButton_Click);
             // 
-            // comboBox1
+            // comboBox
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Test",
-            "en effet ouais",
-            "en effet ouais haha"});
-            this.comboBox1.Location = new System.Drawing.Point(13, 13);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(341, 23);
-            this.comboBox1.TabIndex = 1;
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Items.AddRange(new object[] {
+            "10 clicks"});
+            this.comboBox.Location = new System.Drawing.Point(13, 13);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(341, 23);
+            this.comboBox.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(341, 103);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(13, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "aa";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(366, 450);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox);
             this.Controls.Add(this.clickableButton);
             this.Name = "Form1";
+            this.Tag = "";
             this.Text = "CClicker";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button clickableButton;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -6,12 +6,17 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace CClick
 {
     public partial class Form1 : Form
     {
+        private bool running = false;
+        private int clickCounter = 0;
+        private Stopwatch watcher = new Stopwatch();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +24,52 @@ namespace CClick
 
         private void clickableButton_Click(object sender, EventArgs e)
         {
+            if (!running)
+            {
+                if (comboBox.SelectedIndex != -1)
+                    SetupOnNewRun();
+                else
+                    MessageBox.Show("Please, select a test!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                if (comboBox.SelectedIndex != -1)
+                {
+                    if (comboBox.SelectedIndex == 0)
+                    {
+                        if (clickCounter < 10)
+                        {
+                            clickCounter++;
+                            label1.Text = $"{clickCounter}";
+                            if (clickCounter == 10)
+                            {
+                                richTextBox1.Text += Math.Round(EndTest().TotalSeconds, 3).ToString() + "s\n";
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please, select a test!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+
+        private void SetupOnNewRun()
+        {
+            clickableButton.Text = "CLICK";
+            clickCounter = 0;
+            running = true;
+            watcher.Reset();
+            watcher.Start();
+        }
+
+        private TimeSpan EndTest()
+        {
+            watcher.Stop();
+            clickableButton.Text = "START";
+            running = false;
+            return watcher.Elapsed;
         }
     }
 }
