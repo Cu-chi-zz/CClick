dc3dc9553d74e8e4bca30400194941aaa34b89ad
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-04-22 16:06:34 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿
+namespace CClick
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clickableButton = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // clickableButton
+            // 
+            this.clickableButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(41)))), ((int)(((byte)(41)))));
+            this.clickableButton.FlatAppearance.BorderSize = 0;
+            this.clickableButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(5)))), ((int)(((byte)(5)))));
+            this.clickableButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
+            this.clickableButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.clickableButton.Font = new System.Drawing.Font("Reem Kufi", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.clickableButton.ForeColor = System.Drawing.SystemColors.Window;
+            this.clickableButton.Location = new System.Drawing.Point(12, 194);
+            this.clickableButton.Name = "clickableButton";
+            this.clickableButton.Size = new System.Drawing.Size(342, 244);
+            this.clickableButton.TabIndex = 0;
+            this.clickableButton.Text = "START";
+            this.clickableButton.UseVisualStyleBackColor = false;
+            this.clickableButton.Click += new System.EventHandler(this.clickableButton_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Test",
+            "en effet ouais",
+            "en effet ouais haha"});
+            this.comboBox1.Location = new System.Drawing.Point(13, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(341, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.clickableButton);
+            this.Name = "Form1";
+            this.Text = "CClicker";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clickableButton;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
