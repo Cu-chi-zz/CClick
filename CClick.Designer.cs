317404c345b650d34550a858218f0e0e9ccfbb38
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-05-11 17:02:38 +0200

    Issue: null config can be applied and that's causing an error.

--- CClick.Designer.cs
+++ CClick.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace CClick
 {
-    partial class Form1
+    partial class CClick
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,7 +29,7 @@ namespace CClick
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CClick));
             this.clickableButton = new System.Windows.Forms.Button();
             this.comboBox = new System.Windows.Forms.ComboBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
