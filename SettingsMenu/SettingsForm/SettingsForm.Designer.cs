13c55e0f5ddd2d6f89f2ab159d759c39403def4b
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-05-14 14:42:06 +0200

    (#4) Moved "Save current test" to Settings menu

--- SettingsMenu/SettingsForm/SettingsForm.Designer.cs
+++ SettingsMenu/SettingsForm/SettingsForm.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.soundEffectCheckBox = new System.Windows.Forms.CheckBox();
+            this.saveButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // soundEffectCheckBox
@@ -46,12 +47,28 @@
             this.soundEffectCheckBox.Text = "Enable sound effect on click";
             this.soundEffectCheckBox.UseVisualStyleBackColor = false;
             // 
+            // saveButton
+            // 
+            this.saveButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(130)))), ((int)(((byte)(88)))));
+            this.saveButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(223)))), ((int)(((byte)(224)))), ((int)(((byte)(226)))));
+            this.saveButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(130)))), ((int)(((byte)(88)))));
+            this.saveButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(145)))), ((int)(((byte)(109)))));
+            this.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.saveButton.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.saveButton.Location = new System.Drawing.Point(12, 279);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(325, 28);
+            this.saveButton.TabIndex = 12;
+            this.saveButton.Text = "Save current test as default";
+            this.saveButton.UseVisualStyleBackColor = false;
+            // 
             // SettingsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(42)))), ((int)(((byte)(58)))));
             this.ClientSize = new System.Drawing.Size(349, 319);
+            this.Controls.Add(this.saveButton);
             this.Controls.Add(this.soundEffectCheckBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
@@ -65,5 +82,6 @@
         #endregion
 
         public System.Windows.Forms.CheckBox soundEffectCheckBox;
+        public System.Windows.Forms.Button saveButton;
     }
 }
