59980f5b5b895752e5c9b3d0ea0634dc962d0de7
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-05-14 14:40:34 +0200

    #3 improvment

--- DataManipulation/Stats.cs
+++ DataManipulation/Stats.cs
@@ -2,9 +2,10 @@
 {
     public class StatsData
     {
-        public double ClicksAverage { get; set; }
+        public double ClicksAverage { get; set; } // Clicks per test
         public int TotalClicks { get; set; }
-        public double TotalMsElapsedOnTest { get; set; }
+        public double TotalMsElapsedOnTest { get; set; } //  Ms elapsed on all tests
         public int TotalTests { get; set; }
+        public double ClicksPerSecondsAverage { get; set; }
     }
 }
